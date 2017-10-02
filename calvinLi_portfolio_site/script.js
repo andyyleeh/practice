62205c6f6dd0a1a7754c7f4a292a40ff662443fa
Han Se (Andrew) Lee <andrewhanselee@gmail.com>
2017-10-02 13:57:22 -0400

    smooth scroll added

--- calvinLi_portfolio_site/script.js
+++ calvinLi_portfolio_site/script.js
@@ -9,3 +9,23 @@ os.on('enter', '#bar2', function(element, event){
 os.on('enter', '#bar3', function(element, event){
     element.style.width = '84%';
 })
+
+
+var hashTagActive = "";
+    $(".scroll").on("click touchstart" , function (event) {
+        if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+            event.preventDefault();
+            //calculate destination place
+            var dest = 0;
+            if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+                dest = $(document).height() - $(window).height();
+            } else {
+                dest = $(this.hash).offset().top;
+            }
+            //go to destination
+            $('html,body').animate({
+                scrollTop: dest
+            }, 1500, 'swing');
+            hashTagActive = this.hash;
+        }
+    });
